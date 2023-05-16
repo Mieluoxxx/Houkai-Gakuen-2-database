8b963ebb5d78afda7450727bfd4e1819bee9d7e5
moguw <weiyiding0@gmail.com>
2023-05-16 22:27:36 +0800

    2023.5.16

--- Pre_Learn/bysms/sales/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-
-from sales.views import listorders, listcustomers
-
-urlpatterns = [
-    path('orders/', listorders),
-    path('customers/', listcustomers),
-]
