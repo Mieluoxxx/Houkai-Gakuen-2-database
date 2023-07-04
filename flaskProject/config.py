2f6e9d9d587fe2d662792b0a206ed1ad74376817
moguw <weiyiding0@gmail.com>
2023-07-04 19:27:27 +0800

    rm -rf

--- flaskProject/config.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-db = SQLAlchemy()
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '123456'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:mysql123@localhost/flask'
-
